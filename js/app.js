f98ef8fe255c62decadae7342d940a19f398d008
Annonix04 <braseymo@uat.edu>
2024-09-18 22:49:41 -0500

    fixed issues with function scope

--- js/app.js
+++ js/app.js
@@ -16,15 +16,16 @@ function addTaskToList(task) {
     const li = document.createElement('li');
     li.className = 'list-group-item d-flex justify-content-between align-items-center';
     li.textContent = task;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.id = 'delete-button';
+    deleteBtn.className = 'btn btn-danger btn-sm';
+    deleteBtn.textContent = 'Delete';
+    
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    deleteBtn.addEventListener('click', function() {
+        taskList.removeChild(deleteBtn.parentNode);
+    });
 }
-
-const deleteBtn = document.createElement('button');
-deleteBtn.className = 'btn btn-danger btn-sm';
-deleteBtn.textContent = 'Delete';
-
-deleteBtn.addEventListener('click', function() {
-    taskList.removeChild(li);
-});
-
-li.appendChild(deleteBtn);
-taskList.appendChild(li);
